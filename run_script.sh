9899c6d32476c4faf89ee217d1aceaedf821da78
venguru <kin.sh0717@gmail.com>
2020-02-20 20:22:29 +0900

    スクリプト修正

--- run_script.sh
+++ run_script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#script=$1
-echo $#
-#docker run -it -v $(pwd):/home/project -w /home/project math_opt_python bash -c "pipenv run python $1"
+docker run -it -v $(pwd):/home/project -w /home/project math_opt_python bash -c "pipenv run python $1"
